732bcf0ae0f18f17f36ee3fd6b0b4c4dac41ca05
2215778 <dlcoals0223@naver.com>
2022-11-30 17:53:22 +0900

    실습2

--- main.c
+++ main.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#define MAX_NAME 20
+#include <math.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-struct student{
-	int ID;
-	char name[MAX_NAME];
-	double grade;
+struct point{
+    int x;
+    int y;
 };
 
 int main(int argc, char *argv[]) {
-	struct student student1={1003, "ChaeminLee", 4.3}; //instance
-	
-	student1.ID = 1020;
-	strcpy(student1.name, "LeeChaemin");
-	student1.grade = 3.3;
-	
-	printf("ID : %i\n", student1.ID);
-	printf("name : %s\n", student1.name);
-	printf("ID : %lf\n", student1.grade);
-	
-	return 0;
+    struct point p1, p2;
+    int xdiff, ydiff;
+    double dist;
+   
+    printf("input p1 coordinate (x y) : ");
+    scanf("%d %d", &p1.x, &p1.y);
+   
+    printf("input p2 coordinate (x y) : ");
+    scanf("%d %d", &p2.x, &p2.y);
+    
+    xdiff=p2.x-p1.x; //calculate xdiff
+    ydiff=p2.y-p1.y; //calculate ydiff
+    
+    //피타고라스 정리 sqrt(xdiff^2+ydiff^2) -> dist 
+    dist=sqrt(xdiff*xdiff+ydiff*ydiff);
+    
+	printf("distance : %lf\n", dist);
+    
+    return 0;
 }
 
